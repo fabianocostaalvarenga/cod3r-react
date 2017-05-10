10347a5bc6361ebc2a9a37a9ee4c97ac6f37a0db
Fabiano Costa de Alvarenga <fabiano.concrete@corp.globo.com>
2017-05-10 14:08:35 -0300

    exercícios react-redux

---
+++ react-redux/ex/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import Field from './field'
+
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
+ReactDom.render(
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Fabiano' />
+    </Provider>    
+, document.getElementById('app'))
+
